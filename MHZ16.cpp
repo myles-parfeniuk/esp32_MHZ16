9b384591c1c405877b674e86c5d1d19f7ec04430
myles-parfeniuk <myles.parfenyuk@gmail.com>
2024-08-20 14:40:26 -0700

    Formatting

--- MHZ16.cpp
+++ MHZ16.cpp
@@ -14,7 +14,7 @@ MHZ16::MHZ16(mhz16_config_t cfg)
     , warm_sem(xSemaphoreCreateBinary())
     , uart_evt_queue(NULL)
 {
-    uart_config_t uart_cfg = {.baud_rate = 9600U, //as per data sheet only 9600 is acceptable
+    uart_config_t uart_cfg = {.baud_rate = 9600U, // as per data sheet only 9600 is acceptable
             .data_bits = UART_DATA_8_BITS,
             .parity = UART_PARITY_DISABLE,
             .stop_bits = UART_STOP_BITS_1,
@@ -25,21 +25,15 @@ MHZ16::MHZ16(mhz16_config_t cfg)
     ESP_ERROR_CHECK(uart_param_config(cfg.uart_peripheral, &uart_cfg));
     ESP_ERROR_CHECK(uart_set_pin(cfg.uart_peripheral, cfg.io_tx, cfg.io_rx, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
 
-    //132 is minimum possible size for ring buffers (buffer_sz > UART_HW_FIFO_LEN(uart_num))
+    // 132 is minimum possible size for ring buffers (buffer_sz > UART_HW_FIFO_LEN(uart_num))
     ESP_ERROR_CHECK(uart_driver_install(cfg.uart_peripheral, 132, 132, 1, &uart_evt_queue, 0));
 
-    esp_timer_create_args_t warmup_timer_conf =
-    {
-        .callback = warmup_timer_cb,
-        .arg = this, 
-        .dispatch_method = ESP_TIMER_TASK,
-        .name = "MHZ16 warmup timer",
-        .skip_unhandled_events = true
-    };
+    esp_timer_create_args_t warmup_timer_conf = {
+            .callback = warmup_timer_cb, .arg = this, .dispatch_method = ESP_TIMER_TASK, .name = "MHZ16 warmup timer", .skip_unhandled_events = true};
 
     ESP_ERROR_CHECK(esp_timer_create(&warmup_timer_conf, &warmup_timer_hdl));
-    
-    //3 minute warmup time as per datasheet
+
+    // 3 minute warmup time as per datasheet
     ESP_ERROR_CHECK(esp_timer_start_once(warmup_timer_hdl, WARMUP_PERIOD_US));
 }
 
@@ -47,11 +41,11 @@ MHZ16::MHZ16(mhz16_config_t cfg)
  * @brief Enables/disables self calibration mode.
  *
  * Enables automatic 24hr calibration cycle, zero point is 400ppm.
- * 
+ *
  * As per data sheet this mode is only suitable for home/office/warehouse environments.
  * If using sensor in an agricultural or outdoor settings, in extreme temperature (ex. greenhouses or refrigerators) this mode should be disabled.
- * 
- * @param enable True if enabling self calibration mode, false if otherwise. 
+ *
+ * @param enable True if enabling self calibration mode, false if otherwise.
  *
  * @return void, nothing to return
  */
@@ -59,17 +53,10 @@ void MHZ16::set_self_calibration(bool enable)
 {
     std::array<uint8_t, PACKET_SZ> packet;
 
-    assemble_tx_packet(packet, CMD_SET_SELF_CALIBRATE,
-      {
-        enable ? static_cast<uint8_t>(0xA0) : static_cast<uint8_t>(0x00), 
-        0, 
-        0,
-        0,
-        0
-      }
-    );
+    assemble_tx_packet(packet, CMD_SET_SELF_CALIBRATE, {enable ? static_cast<uint8_t>(0xA0) : static_cast<uint8_t>(0x00), 0, 0, 0, 0});
+
     uart_write_bytes(cfg.uart_peripheral, packet.data(), packet.size());
-    vTaskDelay(100/portTICK_PERIOD_MS); //allow some time for MHZ16 to execute command (it is really slow, documentation is poor on this)
+    vTaskDelay(100 / portTICK_PERIOD_MS); // allow some time for MHZ16 to execute command (it is really slow, documentation is poor on this)
 
     measure_CO2(); // flush first measurement
 }
@@ -77,7 +64,7 @@ void MHZ16::set_self_calibration(bool enable)
 /**
  * @brief Sends command to calibrate zero point.
  *
- * Calibrates sensor zero point. 
+ * Calibrates sensor zero point.
  * As per data sheet the sensor should be operated in a 400ppm CO2 environment for at least 20 minutes for this command is executed.
  *
  * @return void, nothing to return
@@ -87,8 +74,9 @@ void MHZ16::calibrate_zero_point()
     std::array<uint8_t, PACKET_SZ> packet;
 
     assemble_tx_packet(packet, CMD_CAL_ZERO_POINT, {0, 0, 0, 0, 0});
+    
     uart_write_bytes(cfg.uart_peripheral, packet.data(), PACKET_SZ);
-    vTaskDelay(100/portTICK_PERIOD_MS); //allow some time for MHZ16 to execute command (it is really slow, documentation is poor on this)
+    vTaskDelay(100 / portTICK_PERIOD_MS); // allow some time for MHZ16 to execute command (it is really slow, documentation is poor on this)
 
     measure_CO2(); // flush first measurement
 }
@@ -96,29 +84,22 @@ void MHZ16::calibrate_zero_point()
 /**
  * @brief Sends command to calibrate span point.
  *
- * Calibrates sensor span point. 
+ * Calibrates sensor span point.
  * As per data sheet the sensor should be operated in span_point ppm CO2 environment for at least 20 minutes for this command is executed.
  * For example, for a span point of 2000ppm, the sensor should be placed inside a controlled 2000ppm CO2 environment for 20 minutes.
  *
- * @param span_point Specified span point in ppm CO2 to calibrate for. 
- * 
+ * @param span_point Specified span point in ppm CO2 to calibrate for.
+ *
  * @return void, nothing to return
  */
 void MHZ16::calibrate_span_point(uint16_t span_point)
 {
     std::array<uint8_t, PACKET_SZ> packet;
 
-    assemble_tx_packet(packet, CMD_CAL_SPAN_POINT,
-      {
-        static_cast<uint8_t>(span_point / 256), 
-        static_cast<uint8_t>(span_point % 256), 
-        0,
-        0,
-        0
-      }
-    );
+    assemble_tx_packet(packet, CMD_CAL_SPAN_POINT, {static_cast<uint8_t>(span_point / 256), static_cast<uint8_t>(span_point % 256), 0, 0, 0});
+
     uart_write_bytes(cfg.uart_peripheral, packet.data(), PACKET_SZ);
-    vTaskDelay(100/portTICK_PERIOD_MS); //allow some time for MHZ16 to execute command (it is really slow, documentation is poor on this)
+    vTaskDelay(100 / portTICK_PERIOD_MS); // allow some time for MHZ16 to execute command (it is really slow, documentation is poor on this)
 
     measure_CO2(); // flush first measurement
 }
@@ -127,31 +108,26 @@ void MHZ16::calibrate_span_point(uint16_t span_point)
  * @brief Sends command to adjust measurement range
  *
  * @param range Specified max range value in ppm CO2 (for ex, 5000 would set the measurement range to 0ppm -> 5000ppm)
- * 
+ *
  * @return void, nothing to return
  */
 void MHZ16::set_detection_range(uint32_t range)
 {
     std::array<uint8_t, PACKET_SZ> packet;
 
-    assemble_tx_packet(packet, CMD_SET_DETECTION_RANGE, 
-        {
-            0, 
-            static_cast<uint8_t>((range & 0xFF000000) >> 3 * 8), 
-            static_cast<uint8_t>((range & 0x00FF0000) >> 2 * 8),
-            static_cast<uint8_t>((range & 0x0000FF00) >> 1 * 8),
-            static_cast<uint8_t>((range & 0x000000FF) >> 0 * 8)
-        }
-    );
+    assemble_tx_packet(packet, CMD_SET_DETECTION_RANGE,
+            {0, static_cast<uint8_t>((range & 0xFF000000) >> 3 * 8), static_cast<uint8_t>((range & 0x00FF0000) >> 2 * 8),
+                    static_cast<uint8_t>((range & 0x0000FF00) >> 1 * 8), static_cast<uint8_t>((range & 0x000000FF) >> 0 * 8)});
+
     uart_write_bytes(cfg.uart_peripheral, packet.data(), PACKET_SZ);
-    vTaskDelay(100/portTICK_PERIOD_MS); //allow some time for MHZ16 to execute command (it is really slow, documentation is poor on this)
+    vTaskDelay(100 / portTICK_PERIOD_MS); // allow some time for MHZ16 to execute command (it is really slow, documentation is poor on this)
 
     measure_CO2(); // flush first measurement
 }
 
 /**
  * @brief Sends command to measure CO2 and returns back result from MHZ16.
- * 
+ *
  * @return The measured CO2 in ppm.
  */
 int16_t MHZ16::measure_CO2()
@@ -159,6 +135,7 @@ int16_t MHZ16::measure_CO2()
     std::array<uint8_t, PACKET_SZ> packet;
 
     assemble_tx_packet(packet, CMD_READ_CO2, {0, 0, 0, 0, 0});
+
     uart_write_bytes(cfg.uart_peripheral, packet.data(), PACKET_SZ);
 
     packet.fill(0);
@@ -172,19 +149,19 @@ int16_t MHZ16::measure_CO2()
 
 /**
  * @brief Block until warmup period has expired (3 minutes after constructor call).
- * 
+ *
  * As per datasheet, MHZ16 requires 3 minutes to warmup after boot before measurements can be considered accurate.
- * 
+ *
  * Ideally, this warmup period should be adjusted or skipped using the RTC to see the last time since boot; but, it is
  * unfair to assume an external battery is connected to RTC pins, therefore a 3 minute timer started at object instantiation is used instead.
  * Please adjust this logic if using an RTC.
- * 
+ *
  * @return void, nothing to return
  */
 void MHZ16::wait_until_warm()
 {
     ESP_LOGI(TAG, "Waiting 3 minutes for sensor warmup period.");
-    //wait until semaphore is given by warmup_timer_cb
+    // wait until semaphore is given by warmup_timer_cb
     xSemaphoreTake(warm_sem, portMAX_DELAY);
     xSemaphoreGive(warm_sem);
     ESP_LOGI(TAG, "Sensor warmup completed.");
@@ -192,17 +169,17 @@ void MHZ16::wait_until_warm()
 
 /**
  * @brief Check if warmup period has expired (3 minutes after constructor call).
- * 
+ *
  * As per datasheet, MHZ16 requires 3 minutes to warmup after boot before measurements can be considered accurate.
- * 
- * @return False if sensor is still heating, true if sensor is ready. 
+ *
+ * @return False if sensor is still heating, true if sensor is ready.
  */
 bool MHZ16::is_warm()
 {
-    if(xSemaphoreTake(warm_sem, 0) != pdFALSE)
+    if (xSemaphoreTake(warm_sem, 0) != pdFALSE)
     {
         xSemaphoreGive(warm_sem);
-        return true; 
+        return true;
     }
 
     return false;
@@ -214,26 +191,26 @@ bool MHZ16::is_warm()
  * @param packet Reference to array to contain packet contents.
  * @param command The desired command to send.
  * @param data 5 byte wide data array to load into packet.
- * 
+ *
  * @return void, nothing to return
  */
 void MHZ16::assemble_tx_packet(std::array<uint8_t, PACKET_SZ>& packet, uint8_t command, const std::array<uint8_t, DATA_SZ> data)
 {
-    uint8_t i = 0; 
+    uint8_t i = 0;
 
-    //assemble header
-    packet[0] = 0xFF;         // start byte
-    packet[1] = 0x01;         // reserved
+    // assemble header
+    packet[0] = 0xFF;    // start byte
+    packet[1] = 0x01;    // reserved
     packet[2] = command; // command byte
 
-    //assemble data
-    for(const uint8_t &data_byte : data)
+    // assemble data
+    for (const uint8_t& data_byte : data)
     {
         packet[3 + i] = data_byte;
         i++;
     }
 
-    //calculate checksum
+    // calculate checksum
     packet[8] = calculate_checksum(packet);
 }
 
@@ -241,8 +218,8 @@ void MHZ16::assemble_tx_packet(std::array<uint8_t, PACKET_SZ>& packet, uint8_t c
  * @brief Calculates checksum of passed packet.
  *
  * @param packet Reference to array containing packet contents.
- * 
- * @return The calculated checksum. 
+ *
+ * @return The calculated checksum.
  */
 uint8_t MHZ16::calculate_checksum(const std::array<uint8_t, PACKET_SZ>& packet)
 {
@@ -261,8 +238,8 @@ uint8_t MHZ16::calculate_checksum(const std::array<uint8_t, PACKET_SZ>& packet)
  * @brief Calculates checksum and verifies if it matches one contained within packet.
  *
  * @param packet Reference to array containing packet contents.
- * 
- * @return True if checksum was valid, false if glitch occurred. 
+ *
+ * @return True if checksum was valid, false if glitch occurred.
  */
 bool MHZ16::verify_checksum(const std::array<uint8_t, PACKET_SZ>& packet)
 {
@@ -275,17 +252,17 @@ bool MHZ16::verify_checksum(const std::array<uint8_t, PACKET_SZ>& packet)
 
 /**
  * @brief Warmup timer callback.
- * 
+ *
  * One shot timer callback, executed 3 minutes after constructor call.
- * As per datasheet MHZ16 has a 3 minute warmup period. 
- * 
+ * As per datasheet MHZ16 has a 3 minute warmup period.
+ *
  * @param arg Pointer to MHZ16 object passed at timer creation, casted to void pointer.
- * 
- * @return void, nothing to return 
+ *
+ * @return void, nothing to return
  */
-void MHZ16::warmup_timer_cb(void *arg)
+void MHZ16::warmup_timer_cb(void* arg)
 {
-    MHZ16 *sensor = static_cast<MHZ16 *>(arg);
+    MHZ16* sensor = static_cast<MHZ16*>(arg);
 
     xSemaphoreGive(sensor->warm_sem);
 }
--- MHZ16.hpp
+++ MHZ16.hpp
@@ -12,17 +12,17 @@
 /// @brief MHZ16 configuration settings structure passed into MHZ16 constructor
 typedef struct mhz16_config_t
 {
-        gpio_num_t io_tx; //TX GPIO (connects to MHZ16 rx pin)
-        gpio_num_t io_rx; //RX GPIO (connects to MHZ16 tx pin)
+        gpio_num_t io_tx;            ///< TX GPIO (connects to MHZ16 rx pin)
+        gpio_num_t io_rx;            //< RX GPIO (connects to MHZ16 tx pin)
         uart_port_t uart_peripheral; ///< UART host peripheral (UART_NUM_0 to UART_NUM_2)
 
         /// @brief Default MHZ16 configuration settings constructor.
         /// To modify default GPIO pins, run "idf.py menuconfig" esp32_MHZ16->GPIO Configuration.
         /// Alternatively, edit the default values in "Kconfig.projbuild"
         mhz16_config_t()
-            : io_tx(static_cast<gpio_num_t>(CONFIG_ESP32_MHZ16_GPIO_TX)) //default: io 17 
-            , io_rx(static_cast<gpio_num_t>(CONFIG_ESP32_MHZ16_GPIO_RX)) //default: io 16 
-            , uart_peripheral(static_cast<uart_port_t>(CONFIG_ESP32_MHZ16_UART_HOST)) //default: UART_NUM_2 
+            : io_tx(static_cast<gpio_num_t>(CONFIG_ESP32_MHZ16_GPIO_TX))              // default: io 17
+            , io_rx(static_cast<gpio_num_t>(CONFIG_ESP32_MHZ16_GPIO_RX))              // default: io 16
+            , uart_peripheral(static_cast<uart_port_t>(CONFIG_ESP32_MHZ16_UART_HOST)) // default: UART_NUM_2
         {
         }
 
@@ -50,25 +50,24 @@ class MHZ16
         bool is_warm();
 
     private:
-
-        static const constexpr uint64_t WARMUP_PERIOD_US = 3*60*(1000*1000); ///< 3 minute sensor warmup period
-        static const constexpr uint8_t PACKET_SZ = 9U; ///< Data packet size
-        static const constexpr uint8_t DATA_SZ = 5U; ///< Data field width within packet (in bytes)
-        static const constexpr uint8_t CMD_READ_CO2 = 0x86; ///< Read CO2 ppm command
-        static const constexpr uint8_t CMD_SET_SELF_CALIBRATE = 0x79; ///< Enable/disable self calibration command.
-        static const constexpr uint8_t CMD_SET_DETECTION_RANGE = 0x99; ///< Set measurement range command.
-        static const constexpr uint8_t CMD_CAL_ZERO_POINT = 0x87; ///< Calibrate zero point command.
-        static const constexpr uint8_t CMD_CAL_SPAN_POINT = 0x88; ///< Calibrate span point command.
+        static const constexpr uint64_t WARMUP_PERIOD_US = 3 * 60 * (1000 * 1000); ///< 3 minute sensor warmup period
+        static const constexpr uint8_t PACKET_SZ = 9U;                             ///< Data packet size
+        static const constexpr uint8_t DATA_SZ = 5U;                               ///< Data field width within packet (in bytes)
+        static const constexpr uint8_t CMD_READ_CO2 = 0x86;                        ///< Read CO2 ppm command
+        static const constexpr uint8_t CMD_SET_SELF_CALIBRATE = 0x79;              ///< Enable/disable self calibration command.
+        static const constexpr uint8_t CMD_SET_DETECTION_RANGE = 0x99;             ///< Set measurement range command.
+        static const constexpr uint8_t CMD_CAL_ZERO_POINT = 0x87;                  ///< Calibrate zero point command.
+        static const constexpr uint8_t CMD_CAL_SPAN_POINT = 0x88;                  ///< Calibrate span point command.
 
         static const constexpr char* TAG = "MHZ16"; ///< Class tag used in serial/ESP_LOG statements.
 
-        mhz16_config_t cfg; ///< Structure containing GPIO and UART settings, initialized with constructor param.
-        SemaphoreHandle_t warm_sem; ///< Semaphore to indicate when warmup period has completed.
-        QueueHandle_t uart_evt_queue; ///< UART evt queue, used by esp-idf uart driver.
-        esp_timer_handle_t warmup_timer_hdl; ///< One-shot 3 minute warmup timer handle. 
+        mhz16_config_t cfg;                  ///< Structure containing GPIO and UART settings, initialized with constructor param.
+        SemaphoreHandle_t warm_sem;          ///< Semaphore to indicate when warmup period has completed.
+        QueueHandle_t uart_evt_queue;        ///< UART evt queue, used by esp-idf uart driver.
+        esp_timer_handle_t warmup_timer_hdl; ///< One-shot 3 minute warmup timer handle.
 
         void assemble_tx_packet(std::array<uint8_t, PACKET_SZ>& packet, uint8_t command, const std::array<uint8_t, DATA_SZ> data);
         uint8_t calculate_checksum(const std::array<uint8_t, PACKET_SZ>& packet);
         bool verify_checksum(const std::array<uint8_t, PACKET_SZ>& packet);
-        static void warmup_timer_cb(void *arg);
+        static void warmup_timer_cb(void* arg);
 };
